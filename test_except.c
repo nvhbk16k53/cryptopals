7fa5d83149270d45e4e358c5586488aabfc182fb
Hiep NV <hiepnv@vccloud.vn>
2014-09-16 11:51:22 +0700

    Test exception and assertion

---
+++ test_except.c
@@ -0,0 +1,46 @@
+#include "assert.h"
+#include "except.h"
+
+struct Except_T exception = {"Exception"}, e = {"exception"};
+
+void test_raise()
+{
+	TRY
+		RAISE(exception);
+	EXCEPT(exception)
+		assert(1);
+	END_TRY;
+}
+
+void test_else()
+{
+	TRY
+		RAISE(exception);
+	EXCEPT(e)
+		assert(0);
+	ELSE
+		assert(1);
+	END_TRY;
+}
+
+void test_multi_exception()
+{
+	TRY
+		RAISE(exception);
+	EXCEPT(e)
+		assert(0);
+	EXCEPT(exception)
+		assert(1);
+	ELSE
+		assert(0);
+	END_TRY;
+}
+
+int main(int argc, char **argv)
+{
+	test_raise();
+	test_else();
+	test_multi_exception();
+
+	return 0;
+}
